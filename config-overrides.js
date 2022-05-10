c8303bc416151b134271dbd9d6099140f86beb0d
lliuwenlong <18801295284@163.com>
2022-05-10 14:06:24 +0800

    feat: 提交reactpc模版

--- config-overrides.js
+++ config-overrides.js
@@ -1,35 +1,20 @@
 /**
  * @file config-overrides.js
- * @author liuwenlong2@tal.com
- * @date 20210406
+ * @author liuwenlong
 */
 
 /* eslint-disable */
 const {
     override,
-    addPostcssPlugins,
-    fixBabelImports,
     addWebpackAlias,
-    setWebpackPublicPath,
-    addWebpackPlugin,
 } = require('customize-cra');
-const pxToViewport = require('./script/pxToViewport.config');
 const version = Date.now();
-const publicPath = '/management-control';
 const path = require("path");
 
 process.env.REACT_APP_SENTRY_RELEASE_VERSION = version;
 
 module.exports = override(
-    addPostcssPlugins([
-        pxToViewport
-    ]),
-    fixBabelImports('import', {
-        libraryName: 'antd-mobile',
-        style: 'css',
-    }),
     addWebpackAlias({
         "@": path.resolve(__dirname, './src'),
     }),
-    process.env.NODE_ENV === 'production' && setWebpackPublicPath(publicPath),  
 );
